46f6f7fce9085d364efedf1474d5c8930e2f9930
DUDU0123 <sdu200115@gmail.com>
2024-10-09 22:53:29 +0530

    sortEmployeeInDescendingOrderByTheirSalary algorithm completed

--- sort_by_salary.dart
+++ sort_by_salary.dart
@@ -2,8 +2,8 @@
 // You have a list of employees where each employee has id,
 // name, and salary. Write a function that sorts the list of
 // employees by their salary in descending order.
-void main(){
-   List<Map<String, dynamic>> employees = [
+void main() {
+  List<Map<String, dynamic>> employees = [
     {'id': 1, 'name': 'Alice', 'salary': 50000},
     {'id': 2, 'name': 'Bob', 'salary': 45000},
     {'id': 3, 'name': 'Charlie', 'salary': 55000},
@@ -11,13 +11,13 @@ void main(){
     {'id': 5, 'name': 'Eve', 'salary': 48000},
   ];
   sortEmployeeInDescendingOrderByTheirSalary(employees: employees);
+  for (var element in employees) {
+    print(element);
+  }
 }
 
-sortEmployeeInDescendingOrderByTheirSalary({required  List<Map<String, dynamic>> employees,}){
-  // Map<Map<String, dynamic>,int> saved = {};
-  List<Map<String, dynamic>> j = [];
-  Set<int> salary = {};
-  for (var map in employees) {
-    final currentEmployeeSalary = map['salary'];
-  }
+sortEmployeeInDescendingOrderByTheirSalary({
+  required List<Map<String, dynamic>> employees,
+}) {
+  employees.sort((a, b) => b['salary'].compareTo(a['salary']));
 }
