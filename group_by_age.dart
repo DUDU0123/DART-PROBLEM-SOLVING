e0137e595016f5264918efbdd801579f018b5670
DUDU0123 <sdu200115@gmail.com>
2024-10-09 13:30:07 +0530

    groupPeopleByAge algorithm completed.

--- group_by_age.dart
+++ group_by_age.dart
@@ -4,18 +4,33 @@
 // Write a function that groups people by age and returns a map
 // where the keys are ages and the values are lists of names.
 
-void main(){
-  
+void main() {
   List<Map<String, dynamic>> people = [
     {'name': 'Alice', 'age': 30},
+    {'name': 'Chris', 'age': 30},
+    {'name': 'Danny', 'age': 25},
     {'name': 'Bob', 'age': 25},
     {'name': 'Charlie', 'age': 35},
+    {'name': 'Robot', 'age': 30},
     {'name': 'David', 'age': 28},
     {'name': 'Eve', 'age': 32},
   ];
-  groupPeopleByAge(people: people);
+ final val = groupPeopleByAge(people: people);
+ print(val);
 }
 
-groupPeopleByAge({required List<Map<String, dynamic>> people,}){
-
+Map<int, List<String>> groupPeopleByAge({
+  required List<Map<String, dynamic>> people,
+}) {
+  Map<int, List<String>> savedAgeMap = {};
+  for (var map in people) {
+    int age = map['age'];
+    String name = map['name'];
+    if (!savedAgeMap.containsKey(age)) {
+      savedAgeMap[age] = [name];
+    }else{
+      savedAgeMap[age]?.add(name);
+    }
+  }
+  return savedAgeMap;
 }
